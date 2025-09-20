6eb02b8c19dca8506716f323f430aeda5a4f5aec
chaman7787 <aman367787@gmail.com>
2025-09-20 22:09:15 +0530

    Add OTP verification to user registration and serve static files

--- index.js
+++ index.js
@@ -20,6 +20,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Serve static files from public directory
+app.use(express.static('public'));
+
 // Routes
 app.use('/api/users', userRoutes);
 app.use('/api/videos', videoRoutes);
